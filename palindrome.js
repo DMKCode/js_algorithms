b3e195fcf2eec983659198a6ecaa927b4d1bbce0
David Kananda <davidkananda@gmail.com>
2017-07-10 06:24:30 +0100

    added basic solution from freeCodeCamp

--- palindrome.js
+++ palindrome.js
@@ -8,3 +8,11 @@ function palindrome(str) {
 }
 
 palindrome("eye");
+
+
+// Credit freeCodeCamp
+// basic solution
+function palindrome(str) {
+  return str.replace(/[\W_]/g, '').toLowerCase() ===
+         str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+}
