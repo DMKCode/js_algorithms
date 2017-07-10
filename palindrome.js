defbd1b05ec1b6ae8610dc9a8f2803282c4fccfc
David Kananda <davidkananda@gmail.com>
2017-07-10 06:26:39 +0100

    added intermediate solution from freeCodeCamp

--- palindrome.js
+++ palindrome.js
@@ -16,3 +16,15 @@ function palindrome(str) {
   return str.replace(/[\W_]/g, '').toLowerCase() ===
          str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
 }
+
+// Credit freeCodeCamp
+// intermediate solution
+function palindrome(str) {
+  str = str.toLowerCase().replace(/[\W_]/g, '');
+  for(var i = 0, len = str.length - 1; i < len/2; i++) {
+    if(str[i] !== str[len-i]) {
+      return false;
+    }
+  }
+  return true;
+}
